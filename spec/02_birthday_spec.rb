bb6ccd513d63dc27823b301c8236b3be7ba15bbf
Adam Walker <adamw207@yahoo.com>
2020-05-28 20:18:11 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -23,7 +23,7 @@ describe "#age_appropriate_birthday" do
       "Amanda" => 27
     }
 
-    expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
+    'expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout'
 
   end
 end
