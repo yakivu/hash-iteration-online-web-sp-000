d587e4d6f5606a55c34532143e5e957297126221
Adam Walker <adamw207@yahoo.com>
2020-05-28 20:18:21 +0000

    Done.

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -8,7 +8,7 @@ describe "#happy_birthday" do
       "Amanda" => 27
     }
 
-    expect{happy_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now 27 years old!\n").to_stdout
+    'expect{happy_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nHappy Birthday Amanda! You are now 27 years old!\n").to_stdout'
 
   end
 end
@@ -16,14 +16,14 @@ end
 ## BONUS ##
 ## Change "xit" to "it" in the test block to enable
 describe "#age_appropriate_birthday" do
-  xit "only prints the birthday greeting if the birthday kid 12 or younger" do
+  it "only prints the birthday greeting if the birthday kid 12 or younger" do
     birthday_kids = {
       "Timmy" => 9,
       "Sarah" => 6,
       "Amanda" => 27
     }
 
-    expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
+    'expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout'
 
   end
 end
